285a6631d02b10d24d27f07f36f6f560fdef349d
Naga-Nag <lordofcarpinchus@gmail.com>
2024-10-12 14:16:41 -0300

    basico completo v1

--- src/lib/components/DlCsv.svelte
+++ src/lib/components/DlCsv.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-    import { downloadCSV } from '$lib/utils';
-    export let data: any;
-    export let placeholder = 'Descargar CSV';
-    export let className = '';
+	import { downloadCSV } from '$lib/utils';
+	export let data: any;
+	export let placeholder = 'Descargar CSV';
+	export let className = '';
 </script>
 
 <main>
-    <button class="btn r:10 p:10 bg:beryl bg:fade-50:hover font:white font:bold cursor:pointer {className}"
-		on:click={() => downloadCSV(data)}>
-        {placeholder}
+	<button
+		class="btn bg:beryl bg:rgb(0,150,105):hover transition:background-color|200ms|ease-in-out font:white {className}"
+		on:click={() => downloadCSV(data)}
+	>
+		{placeholder}
 	</button>
 </main>
--- src/lib/master.css.js
+++ src/lib/master.css.js
@@ -1,6 +1,6 @@
 import { init, Style } from '@master/css';
 Style.extend('classes', {
-    btn: 'font:12 font:semibold font:rgb(0,0,0) bg:rgb(252,252,252) h:40 r:4 b:none p:10|15 mr:10 ml:10 r:2 cursor:pointer',
-    main: 'font-family:Comfortaa bg:rgb(51,87,155) r:12 p:12|15|20',
+    main: 'bg:rgb(51,87,155) r:12 p:12|15|20',
+    btn: 'font:semibold h:40 r:4 b:none p:10|15 mr:10 ml:10 r:2 cursor:pointer',
 })
 init()
