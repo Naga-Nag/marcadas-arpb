a02d4050f3f35980ad3821a6670e71f7f004544f
Naga-Nag <lordofcarpinchus@gmail.com>
2024-11-20 22:41:56 -0300

    fixes

--- sql/MarcadaDelDiaPEAP.sql
+++ sql/MarcadaDelDiaPEAP.sql
@@ -19,12 +19,12 @@ BEGIN
     d.DeptName AS Departamento,
     MAX(CASE WHEN DATEPART(HOUR, ci.CheckTime) >= 13 AND (CAST(ci.CheckTime AS DATE) = @FechaHoy) THEN ci.CheckTime END) AS Salida,
     MIN(CASE WHEN DATEPART(HOUR, ci.CheckTime) <= 10 AND (CAST(ci.CheckTime AS DATE) = @FechaHoy) THEN ci.CheckTime END) AS Entrada,
-    ui.OtherInfo AS Info
-    
+    CAST(ui.OtherInfo AS VARBINARY(MAX)) AS Info
   FROM
     dbo.Userinfo ui
   INNER JOIN dbo.Dept d ON ui.Deptid = d.Deptid
   LEFT JOIN dbo.Checkinout ci ON ui.Userid = ci.Userid
+  
   GROUP BY
     ui.Userid, ui.UserCode, ui.Name, d.DeptName, CAST(ui.OtherInfo AS VARBINARY(MAX))
   RETURN
