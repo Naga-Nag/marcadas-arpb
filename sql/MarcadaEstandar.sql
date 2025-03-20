972be0860ace3d199d77afaeb78d9e2eb8a0f81c
InformaticaARPB <arpbdepainformatica@gmail.com>
2025-03-20 09:03:12 -0600

    fixes

--- sql/MarcadaEstandar.sql
+++ sql/MarcadaEstandar.sql
@@ -20,14 +20,19 @@ BEGIN
           ui.Name AS Nombre,
           d.DeptName AS Departamento,
           MAX(CASE WHEN DATEPART(HOUR, ci.CheckTime) >= 13 AND (CAST(ci.CheckTime AS DATE) = DATEADD(DAY, -1, @FechaHoy)) THEN ci.CheckTime END) AS Salida,
-          MIN(CASE WHEN DATEPART(HOUR, ci.CheckTime) <= 10 AND (CAST(ci.CheckTime AS DATE) = @FechaHoy) THEN ci.CheckTime END) AS Entrada,
+          MIN(CASE 
+        WHEN DATEPART(HOUR, ci.CheckTime) <= 10
+               AND (CAST(DATEADD(HOUR, -6, ci.CheckTime) AS DATE) = @FechaHoy) 
+        THEN ci.CheckTime 
+        ELSE NULL 
+    END) AS Entrada,
           ui.CUIL AS CUIL,
           ui.Jornada AS Jornada,
           ui.Activo AS Activo
      FROM
           dbo.Userinfo ui
-     INNER JOIN dbo.Dept d ON ui.Deptid = d.Deptid
-     LEFT JOIN dbo.Checkinout ci ON ui.Userid = ci.Userid
+          INNER JOIN dbo.Dept d ON ui.Deptid = d.Deptid
+          LEFT JOIN dbo.Checkinout ci ON ui.Userid = ci.Userid
      WHERE d.DeptName = @Departamento
      GROUP BY
           ui.Userid, ui.UserCode, ui.Name, d.DeptName, ui.CUIL, ui.Jornada, ui.Activo
